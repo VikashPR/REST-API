6a2528ad1d10d149ec9c19b476d754273fdef1c6
VikashPR <vikashrajendran2806@gmail.com>
2022-08-05 23:02:43 +0530

    feat: router template add

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to DB'));
 
-const PORT = process.env.PORT || 3000;
+// Accept Json object
+app.use(express.json())
 
+const subscribersRouter = require('./router/subscribers')
+app.use('./subscribers',subscribersRouter)
+
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
