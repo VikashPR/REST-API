f4dca46ee1352c71978cd4c133fecabaab295766
VikashPR <vikashrajendran2806@gmail.com>
2022-08-05 22:46:48 +0530

    feat: db connection add

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const mongoose = require('mongoose');
+
+// *process.env.{anything} can access any variable from the .env file
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to DB'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
