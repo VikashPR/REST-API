a0f136a153804bc07af7fb5951b6a485c389af8c
VikashPR <vikashrajendran2806@gmail.com>
2022-08-07 11:04:53 +0530

    feat: delete api route add

--- router/subscribers.js
+++ router/subscribers.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const subscriber = require('../models/subscriber');
 const router = express.Router();
 const Subscriber = require('../models/subscriber');
 
@@ -13,8 +14,8 @@ router.get('/', async (req, res) => {
 });
 
 // Getting one
-router.get('/:id', (req, res) => {
-  res.send(req.params.id);
+router.get('/:id', getSubscriber, (req, res) => {
+  res.json(res.subscriber);
 });
 
 // Creating one
@@ -32,8 +33,34 @@ router.post('/', async (req, res) => {
 });
 
 // Updating one
-router.patch('/:id', (req, res) => {});
+router.patch('/:id', getSubscriber, (req, res) => {});
 
 // Deleting one
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json({ msg: 'Deleted subscriber' });
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
+
+// Middle ware
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ msg: 'cannot find the user ' });
+    }
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+  res.subscriber = subscriber;
+  next();
+}
+
+
+
+
 module.exports = router;
